d9ab9296628183f1f2551f6a6d8935e13aa3f978
Tolga Özen <mtolgaozen@gmail.com>
2021-01-15 02:18:51 +0300

    Create conditional.go

---
+++ middlewares/conditional.go
@@ -0,0 +1,50 @@
+package GMiddleware
+
+import (
+	"github.com/labstack/echo/v4"
+	"gotham/helpers"
+	"net/http"
+)
+
+type IMiddleware interface {
+	control(c echo.Context) (bool, error)
+}
+
+// Conditional Middlewares
+func Or(middleware []IMiddleware) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			var msg = "You cannot access"
+
+			for _, m := range middleware {
+				canDo, err := m.control(c)
+				if err != nil {
+					msg = err.Error()
+				}
+				if canDo {
+					return next(c)
+				}
+			}
+			return c.JSON(http.StatusBadRequest, helpers.ErrorResponse(http.StatusBadRequest, msg))
+		}
+	}
+}
+
+func And(middleware []IMiddleware) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			var msg = "You cannot access"
+
+			for _, m := range middleware {
+				canDo, err := m.control(c)
+				if err != nil {
+					msg = err.Error()
+				}
+				if !canDo {
+					return c.JSON(http.StatusBadRequest, helpers.ErrorResponse(http.StatusBadRequest, msg))
+				}
+			}
+			return next(c)
+		}
+	}
+}
