4d1c286ba73ae2f118b167c1fee7bd186d905ca9
Tolga Özen <mtolgaozen@gmail.com>
2021-05-06 21:18:34 +0300

    Update userRepository.go

--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -12,10 +12,20 @@ type IUserRepository interface {
 	IMigrate
 	ISeed
 
-	GetUserByID(id uint) (models.User, error)
+	GetUserByID(ID uint) (models.User, error)
 	GetUserByEmail(email string) (models.User, error)
-	GetUsers(pagination *scopes.Pagination) ([]models.User, error)
-	GetUsersCount() (int64, error)
+
+	// Getter Options
+	GetUsersWithPagination(userIDs []uint, pagination *scopes.Pagination) ([]models.User, error)
+
+	// Create & Save & Updates & Delete
+	Create(user *models.User) (err error)
+	Save(user *models.User) (err error)
+	Updates(user *models.User, updates map[string]interface{}) (err error)
+	Delete(user *models.User) (err error)
+
+	// Getters
+	GetUserIDs() (userIDs []uint, err error)
 }
 
 type UserRepository struct {
@@ -27,7 +37,6 @@ type UserRepository struct {
  *
  * @return error
  */
-
 func (repository *UserRepository) Seed() (err error) {
 	for i := 0; i < 50; i++ {
 		hashedPassword, _ := helpers.Hash("password")
@@ -37,6 +46,8 @@ func (repository *UserRepository) Seed() (err error) {
 			Email: faker.Internet().Email(),
 			Password: string(hashedPassword),
 			Image: &image ,
+			Admin: true,
+			Verified: true,
 		}
 
 		if err := repository.DB().Create(&user).Error; err != nil {
@@ -56,9 +67,13 @@ func (repository *UserRepository) Migrate() (err error) {
 	return repository.DB().AutoMigrate(models.User{})
 }
 
+func (repository *UserRepository) GetUsersWithPagination(userIDs []uint, pagination *scopes.Pagination) (users []models.User, err error) {
+	err = repository.DB().Scopes(pagination.Paginate("users","id", []interface{}{userIDs},"created_at", "updated_at")).Find(&users).Error
+	return
+}
 
-func (repository *UserRepository) GetUserByID(id uint) (user models.User, err error) {
-	err = repository.DB().First(&user, id).Error
+func (repository *UserRepository) GetUserByID(ID uint) (user models.User, err error) {
+	err = repository.DB().First(&user, ID).Error
 	return
 }
 
@@ -67,12 +82,34 @@ func (repository *UserRepository) GetUserByEmail(email string) (user models.User
 	return
 }
 
-func (repository *UserRepository) GetUsers(pagination *scopes.Pagination) (users []models.User, err error) {
-	err = repository.DB().Scopes(pagination.Paginate("users","created_at", "updated_at")).Find(&users).Error
+/**
+ * Create & Update & Delete
+ *
+ */
+
+func (repository *UserRepository) Create(user *models.User) (err error) {
+	return repository.DB().Create(user).Error
+}
+
+func (repository *UserRepository) Save(user *models.User) (err error) {
+	return repository.DB().Save(user).Error
+}
+
+func (repository *UserRepository) Updates(user *models.User, updates map[string]interface{}) (err error) {
+	return repository.DB().Model(user).Updates(updates).Error
+}
+
+func (repository *UserRepository) Delete(user *models.User) (err error) {
+	return repository.DB().Delete(user).Error
+}
+
+/**
+ * Getters
+ *
+ */
+
+func (repository *UserRepository) GetUserIDs() (userIDs []uint, err error) {
+	err = repository.DB().Model(&models.User{}).Pluck("id", &userIDs).Error
 	return
 }
 
-func (repository *UserRepository) GetUsersCount() (count int64, err error) {
-	err = repository.DB().Model(&models.User{}).Count(&count).Error
-	return
-}
