d84710f766a242b945778d986f3f84470c1eede1
Tolga Özen <mtolgaozen@gmail.com>
2021-05-06 21:18:39 +0300

    Update pagination.go

--- models/scopes/pagination.go
+++ models/scopes/pagination.go
@@ -1,7 +1,9 @@
 package scopes
 
 import (
+	"fmt"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"gotham/helpers"
 )
 
@@ -12,8 +14,8 @@ type Pagination struct {
 	SortBy  string `query:"sort_by"`
 }
 
-func (r *Pagination) Paginate(tableName string ,orderBy ...string) func(db *gorm.DB) *gorm.DB {
-	return func (db *gorm.DB) *gorm.DB {
+func (r *Pagination) Paginate(tableName string, defaultField string, vars []interface{}, orderBy ...string) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
 		if r.Page <= 0 {
 			r.Page = 1
 		}
@@ -22,14 +24,20 @@ func (r *Pagination) Paginate(tableName string ,orderBy ...string) func(db *gorm
 			r.Limit = 20
 		}
 
-		if !helpers.InArray(r.OrderBy, orderBy) {
-			r.OrderBy = "id"
-		}
-
 		if !helpers.InArray(r.SortBy, []string{"asc", "desc"}) {
 			r.SortBy = "asc"
 		}
 
-		return db.Order("`" + tableName + "`" + "." +"`" + r.OrderBy + "` " + r.SortBy).Offset(helpers.OffsetCal(r.Page, r.Limit)).Limit(r.Limit)
+		if !helpers.InArray(r.OrderBy, orderBy) {
+			return db.Clauses(clause.OrderBy{
+				Expression: clause.Expr{SQL: fmt.Sprintf("FIELD(`%v`.`%v`,?) %v", tableName, defaultField, r.SortBy), Vars: vars, WithoutParentheses: true},
+			}).Offset(helpers.OffsetCal(r.Page, r.Limit)).Limit(r.Limit)
+		}
+
+		if tableName == "" {
+			return db.Order(fmt.Sprintf("`%v` %v", r.OrderBy, r.SortBy)).Offset(helpers.OffsetCal(r.Page, r.Limit)).Limit(r.Limit)
+		} else {
+			return db.Order(fmt.Sprintf("`%v`.`%v` %v", tableName, r.OrderBy, r.SortBy)).Offset(helpers.OffsetCal(r.Page, r.Limit)).Limit(r.Limit)
+		}
 	}
 }
